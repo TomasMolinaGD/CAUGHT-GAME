2bab62527a313abdff49f098aa7b89f4f489e69b
Tomas Molina <tomasmolinarc@gmail.com>
2026-09-01 19:31:21 -0300

    Segundo Commit
    se incluyo programación del enemigo que sigue al personaje, modelos y animaciones a aplicar

---
+++ Assets/Scripts/Alien/AgentSteering.cs
@@ -0,0 +1,51 @@
+
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class AgentSteering : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float _speed = 5;
+    [SerializeField] private float _maxSteering = 5;
+    private Vector3 _velocity;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Seek();
+        //Flee();
+        Vector3 direction = target.position - transform.position;
+        
+        transform.position += direction.normalized *_speed * Time.deltaTime;
+        transform.forward = _velocity;
+    }
+
+    private void Seek() // --> Persigue 
+    {
+        Vector3 desired = (target.position - transform.position).normalized;
+        desired *= _speed;
+        //Steering =  desiredVelocity - CurrentVelocity;
+        Vector3 steering = desired - _velocity;
+        //clamp -> Var A = MathF.Clamp (-5,0,10) -> limitar una variable entre 2 factores 
+        steering = Vector3.ClampMagnitude(steering, _maxSteering * Time.deltaTime);
+
+        _velocity += steering; 
+    }
+
+     /*private void Flee() //--> escapa
+    {
+        Vector3 desired = (transform.position - transform.position).normalized;
+        desired *= _speed;
+        //Steering =  desiredVelocity - CurrentVelocity;
+        Vector3 steering = desired - _velocity;
+        //clamp -> Var A = MathF.Clamp (-5,0,10) -> limitar una variable entre 2 factores 
+        steering = Vector3.ClampMagnitude(steering, _maxSteering * Time.deltaTime);
+
+        _velocity += steering; 
+    }*/
+
+}
